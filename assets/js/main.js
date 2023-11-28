747f1624013b2e158e435f187944a4961e13b688
Riyaa-k <anshitakoshta28@gmail.com>
2023-11-28 23:45:07 +0530

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,7 +78,7 @@ function navHighlighter()
 
 // typing text animation script
 var typed = new Typed("#element", {
-  strings: ["Developer", "Designer"],
+  strings: ["Software Developer", "Full-Stack Developer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true
